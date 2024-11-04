4eb49f5dc7d0659374322680a1617a4a4d27e22f
EdmundNyaribo <nyariboedmund11@gmail.com>
2024-11-04 15:29:31 +0300

    initialize repository

---
+++ src/main/java/com/real/medical_supplies/MedicalSuppliesApplication.java
@@ -0,0 +1,13 @@
+package com.real.medical_supplies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicalSuppliesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicalSuppliesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/real/medical_supplies/MedicalSuppliesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.real.medical_supplies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicalSuppliesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
